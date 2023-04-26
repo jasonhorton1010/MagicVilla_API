ba2fde190bb27b4640856490b00405b9527c344c
Jason Horton <jasonhorton1010@googlemail.com>
2023-04-26 17:21:56 +0100

    Configured AutoMapper

---
+++ MagicVilla_WebProject/MappingConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MagicVilla_WebProject.Models;
+using MagicVilla_WebProject.Models.Dto;
+using MMagicVilla_WebProjectModels.Dto;
+
+namespace MagicVilla_WebProject;
+
+public class MappingConfig : Profile
+{
+    public MappingConfig()
+    {
+
+        CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+        CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+
+        CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+        CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+    }
+}
--- MagicVilla_WebProject/Program.cs
+++ MagicVilla_WebProject/Program.cs
@@ -1,7 +1,11 @@
+using MagicVilla_WebProject;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAutoMapper(typeof(MappingConfig))>;
 
 var app = builder.Build();
 
