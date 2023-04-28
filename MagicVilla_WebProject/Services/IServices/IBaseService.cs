b9cdf59f208cd1df93af911b67f7b32c40b1483a
Jason Horton <jasonhorton1010@googlemail.com>
2023-04-28 14:16:29 +0100

    Implemented BaseService

---
+++ MagicVilla_WebProject/Services/IServices/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿using MagicVilla_WebProject.Models;
+
+namespace MagicVilla_WebProject.Services.IServices
+{
+    public interface IBaseService
+    {
+        public APIResponse responseModel{ get; set; }
+
+        Task<T> SendAsync<T>(APIRequest aPIRequest);
+    }
+}
