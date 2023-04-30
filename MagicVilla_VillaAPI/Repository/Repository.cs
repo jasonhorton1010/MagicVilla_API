0eaec314eec1befa36194ea554b9f83d8c7e59d1
Jason Horton <jasonhorton1010@googlemail.com>
2023-04-30 14:54:36 +0100

    Including Villa when getting Villa Numbers

--- MagicVilla_VillaAPI/Repository/Repository.cs
+++ MagicVilla_VillaAPI/Repository/Repository.cs
@@ -3,6 +3,7 @@ using MagicVilla_VillaAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
 using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 
 namespace MagicVilla_VillaAPI.Repository
 {
@@ -22,7 +23,7 @@ namespace MagicVilla_VillaAPI.Repository
             await SaveAsync();
         }
 
-        public async Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true)
+        public async Task<T> GetAsync(Expression<Func<T, bool>> filter = null, bool tracked = true, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
 
@@ -36,16 +37,32 @@ namespace MagicVilla_VillaAPI.Repository
                 query = query.Where(filter);
             }
 
+            if(includeProperties != null)
+            {
+                foreach(var includeProp in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+
             return await query.FirstOrDefaultAsync();
         }
 
-        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter = null)
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> filter = null, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
 
             if (filter != null)
             {
                 query = query.Where(filter);
+            }
+
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
             }
 
             return await query.ToListAsync();
