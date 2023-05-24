045f7899fc4a46ce2c28cd1698e7ca3c4b36eeaf
Jason Horton <jasonhorton1010@googlemail.com>
2023-05-24 17:38:47 +0100

    User Repo - Login

---
+++ MagicVilla_VillaAPI/Models/Dto/UserDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace MagicVilla_VillaAPI.Models.Dto
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Name { get; set; }
+    }
+}
